45d2b60a8d9a9f43bb3638574a500cb657c26d76
TrStans606 <100236022+TrStans606@users.noreply.github.com>
2023-10-14 01:44:57 -0400

    Update foodXOpenMapApi.py

--- foodXOpenMapApi.py
+++ foodXOpenMapApi.py
@@ -1,4 +1,4 @@
-#these are the key packages I am installing to generate and anaylze the data
+#These are the key packages I am installing to generate and anaylze the data
 #I am using OpenStreetMap as it is free and doesn't require card verification
 
 from OSMPythonTools.nominatim import Nominatim
@@ -12,11 +12,6 @@ def areaIdGrabber(cityName):
     nominatim = Nominatim()
     areaId = nominatim.query(cityName).areaId()
     return areaId
-
-#api = Api()
-#way = api.query('relation/1812962')
-
-#print(way.tag('boundary'))
 
 #I made the city varible easily accessible so it could be eaisly changed
 city = 'Indianapolis, Indiana'
